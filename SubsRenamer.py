c793130939b8e13d6c72f7df8b72ab8f20bcf1d4
Leo2660 <leonardosolari67@gmail.com>
2022-12-17 15:58:50 +0100

    Add LICENSE

--- SubsRenamer.py
+++ SubsRenamer.py
@@ -1,3 +1,21 @@
+# SubsRenamer
+# Copyright (C) 2022  Leonardo Solari
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 from models import files, RARBGmovies, RARBGtvshows
 import os
 from os import name
@@ -28,9 +46,9 @@ if __name__ == "__main__":
     banner()
     print("What subtitles are we working with?")
     print("(1) - RARBG movie\n(2) - RARBG tv show")
-    mode = input("Choose a mode: ")
+    mode = input("Choose a model: ")
     
-    print("Choose the directory")
+    print("Choose the directory containing the movie(s) or the tv show(s)")
     directory = files.askForDirectory()
 
     clear()
