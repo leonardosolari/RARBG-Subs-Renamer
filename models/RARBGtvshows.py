f8afcfdbc9261b148c9afa9494da272e394e9790
Leo2660 <leonardosolari67@gmail.com>
2023-01-08 14:29:59 +0100

    Update substrings

--- models/RARBGtvshows.py
+++ models/RARBGtvshows.py
@@ -26,18 +26,19 @@ def run(directory):
     os.mkdir(renamed_directory)
     prev = ""
     count = 0
-    substring = "english"
-    for file in filelist:
-        if (substring not in file.lower()):
-            continue
-        parent_directory_name = files.fileNameFromPath((os.path.abspath(os.path.join(file, os.pardir))))
-        if prev == parent_directory_name:
-            count += 1
-        else:
-            count = 0
-        new_location = renamed_directory + "/" + parent_directory_name + "-" + str(count) + ".srt"
-        shutil.copyfile(file, new_location)
-        prev = parent_directory_name
+    substrings = ["english", "eng", "italian", "ita"]
+    for substring in substrings:
+        for file in filelist:
+            if (substring not in file.lower()):
+                continue
+            parent_directory_name = files.fileNameFromPath((os.path.abspath(os.path.join(file, os.pardir))))
+            if prev == parent_directory_name:
+                count += 1
+            else:
+                count = 0
+            new_location = renamed_directory + "/" + parent_directory_name + "-" + str(count) + "-" + substring + ".srt"
+            shutil.copyfile(file, new_location)
+            prev = parent_directory_name
 
     print("")
     print("Subtitles have been renamed and copied in " + colored(renamed_directory, "green"))
